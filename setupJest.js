a98dea7e70d2a82ca2fa9812dba48cd43d8f7f52
Khoa Bui Hoang Dang <khoa.bui@grabtaxi.com>
2020-07-15 02:06:37 +0700

    update jest setup file

--- setupJest.js
+++ setupJest.js
@@ -1,1 +1,13 @@
+import EN from 'public/locales/en';
+
+// Mock localStorage as it is a browser thing and not available in the test environment (vDOM)
+global.localStorage = require('jest-localstorage-mock');
+
+// Mock fetch
 global.fetch = require('jest-fetch-mock');
+
+// Setup I18n
+const I18n = require('i18n-js');
+
+I18n.translations.en = EN;
+I18n.locale = 'en';
