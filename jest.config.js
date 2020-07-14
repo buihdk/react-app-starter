8f4ee81feda9413dc3a33b7d646296122bc5f66c
Khoa Bui <contact@buihdk.com>
2020-07-14 18:23:46 +0700

    add modulePaths

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
   coverageDirectory: '<rootDir>/coverage/',
   coverageReporters: ['lcov', 'text', 'text-summary'],
   moduleFileExtensions: ['js', 'jsx'],
+  modulePaths: ['node_modules', '<rootDir>', '<roots>'],
   setupFilesAfterEnv: ['./setupJest.js', './setupEnzyme.js'],
   testEnvironment: 'jest-environment-jsdom-fourteen',
   testMatch: [
