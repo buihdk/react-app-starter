97995edfc9b53ed0c3cdfa8af95a76a57339b52c
Khoa Bui <buihdk@Khoas-MacBook-Pro.local>
2023-06-08 15:53:28 -0400

    migrate BrowserRouter to RouterProvider

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,17 +1,29 @@
 import React, { lazy, memo } from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { createBrowserRouter, useRouteError, Routes, Route, RouterProvider } from 'react-router-dom';
 
 const HomePage = lazy(() => import('pages/Home'));
 const UserPage = lazy(() => import('pages/User'));
 const NotFoundPage = lazy(() => import('pages/NotFound'));
 
-const RoutesV6 = () => (
+const ErrorBoundary = () => {
+  const error = useRouteError();
+  console.error(('Error logged: ', error));
+  return <div>Dang!</div>;
+};
+
+const Root = () => (
   <Routes>
-    <Route path="/" element={<Navigate replace to="/home" />} />
-    <Route path="home" element={<HomePage />} />
-    <Route path="mock-users" element={<UserPage />} />
+    <Route path="/home" element={<HomePage />} errorElement={<ErrorBoundary />} />
+    <Route path="/mock-users" element={<UserPage />} errorElement={<ErrorBoundary />} />
     <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
+
+const router = createBrowserRouter([
+  { path: '/', Component: HomePage },
+  { path: '*', Component: Root },
+]);
+
+const RoutesV6 = () => <RouterProvider router={router} />;
 
 export default memo(RoutesV6);
